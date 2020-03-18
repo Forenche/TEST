899014bd04e393c1add636ed228486105d030902
Rahul <>
2020-03-18 22:33:03 +0530

    Added mako hotplug

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 18 09:13:47 IST 2020"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 18 22:15:30 IST 2020"
 #define LINUX_COMPILE_BY "forenche"
 #define LINUX_COMPILE_HOST "Forenche"
 #define LINUX_COMPILER "Android (6170260 based on r370808b) clang version 10.0.2 (https://android.googlesource.com/toolchain/llvm-project 122c8c71fd2978587324fd3c7ccbda7984520828)"
